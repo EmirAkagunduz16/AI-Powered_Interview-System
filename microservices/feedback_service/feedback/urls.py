f5dfe05159c36d2b6f0add4334ff993a4cd90260
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2025-06-30 14:59:33 +0300

    gitignore file was created

--- microservices/feedback_service/feedback/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('health/', views.feedback_service_health, name='feedback_service_health'),
-    path('feedback/', views.feedback_list, name='feedback_list'),
-] 
--- microservices/interview_service/interviews/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('health/', views.interview_service_health, name='interview_service_health'),
-    path('interviews/', views.interviews_list, name='interviews_list'),
-] 
